4bb11e33d8a22b6f18057dfcd83891f58eae1913
Raviraj Chauhan <rjchauhan8427@gmail.com>
2021-07-28 15:34:29 +0530

    slideover example moved to new route

---
+++ resources/views/slideover.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Slideover') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div>
+                <button onclick="Livewire.emit('ManagePost.slideover.toggle')" type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Create Post
+                </button>
+
+                <button onclick="Livewire.emit('ManageBlog.slideover.toggle')" type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Create Blog
+                </button>
+            </div>
+
+            <livewire:manage-post/>
+            <livewire:manage-blog/>
+        </div>
+    </div>
+</x-app-layout>
